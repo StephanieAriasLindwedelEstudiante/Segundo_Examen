7159bec6bd8bf0d9588fbad2a40550381c627383
StephanieAriasLindwedelEstudiante <step2@STEPHANIE_ARIAS>
2025-07-25 21:07:30 -0600

    Elimina páginas About y Contact; añade inicio de sesión
    Se eliminaron las páginas About.aspx y Contact.aspx, junto con sus archivos de código subyacente. Se añadió una nueva clase Usuarios en Usuarios.vb con propiedades relevantes. Se actualizó el archivo de proyecto para incluir login.aspx y su código subyacente, eliminando las referencias a las páginas eliminadas. Se modificó el título en Site.Master y se actualizaron los enlaces de navegación para incluir Login y Clientes. Se creó la nueva página de inicio de sesión con campos para correo y contraseña, y se implementó la lógica básica para su funcionamiento.

---
+++ MODELS/Usuarios.vb
@@ -0,0 +1,13 @@
+﻿Public Class Usuarios
+    Public Property Id As Integer
+    Public Property Nombre As String
+    Public Property Correo As String
+    Public Property Contrasena As String
+    Public Property Activo As Boolean
+    Public Property Rol_ID As Integer
+    Public Sub New()
+    End Sub
+    Public Function dtToUsuarios(dt As DataTable) As List(Of Usuarios)
+
+    End Function
+End Class
